1e9473e6c6523cbfc72f9983c485a5f921c2a317
Mohammadreza <mreza.khansari@gmail.com>
2024-02-02 12:52:04 +0330

    create api project

---
+++ SampleCRUD/src/API/SampleCRUD.API/APIServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System.Reflection;
+
+namespace SampleCRUD.API;
+
+public static class APIServiceRegistration
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
---
+++ SampleCRUD/src/API/SampleCRUD.API/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+
+namespace SampleCRUD.API.MappingProfiles;
+
+public class ProductProfile : Profile
+{
+    public ProductProfile()
+    {
+    }
+}
